dad6f28f3c9f533180c0fd7b6686d997a689ac69
Yarudv <102300461+Yarudv@users.noreply.github.com>
2022-05-04 13:43:25 -0400

    Actualizacion break ciclo
    actualizacion de corte de ciclo

--- clase4.py
+++ clase4.py
@@ -14,24 +14,26 @@ while True:
     controltime(time.time()+3,"Ejecutando aplicación de compra")
     compra_producto = int(input("¿Qué producto desea comprar? (1/2):"))
     if compra_producto == 1:
-        cantidad_1 = int(input("Ingrese cantidad a comprar del producto_1: " ))
+        cantidad_1 = int(input("Ingrese cantidad a comprar del producto 1: " ))
         stock_producto_1 = stock_producto_1 - cantidad_1
-        print(stock_producto_1)
+        print("El stock actual del producto 1 es: ",stock_producto_1)
         if stock_producto_1 <= 100:
             acu1 = stock_producto_1 + 50
             stock_producto_1 = acu1
     else:
-        cantidad_2 = int(input("Ingrese cantidad a comprar del producto_2: " ))
+        cantidad_2 = int(input("Ingrese cantidad a comprar del producto 2: " ))
         stock_producto_2 = stock_producto_2 -cantidad_2
-        print(stock_producto_2)
+        print("El stock actual del producto 2 es: ", stock_producto_2)
         if stock_producto_2 <= 100:
             acu2 = stock_producto_2 + 50
             stock_producto_2 = acu2
     contador = contador + 1
-    if contador >= 10:
-        print("Stock producto_1 es:",stock_producto_1)
-        print("Stock producto_2 es:",stock_producto_2)         
-       
+    if contador == 10:
+        print("Stock producto 1 es:",stock_producto_1)
+        print("Stock producto 2 es:",stock_producto_2)         
+    elif contador == 11:
+        print("Su compra ha finalizado")
+        break 
 
 
 
